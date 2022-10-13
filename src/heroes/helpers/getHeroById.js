00f2efd3b9ab592b0860be4ce10bd969bc5ee8c0
Carolina <carolinazapatavidal@gmail.com>
2022-10-13 19:16:50 -0300

    first commit

---
+++ src/heroes/helpers/getHeroById.js
@@ -0,0 +1,8 @@
+import { heroes } from "../data/heroes"
+
+
+export const getHeroById = (id) => {
+
+  return heroes.find(hero => hero.id === id); 
+  //si find no encuentra nada, devuelve undefined
+}
